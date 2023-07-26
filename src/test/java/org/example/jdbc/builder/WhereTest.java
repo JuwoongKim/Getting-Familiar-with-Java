7600d0c6c5ef6008696b33b7ddff3e925800d068
Juwoong <wwong7247@gmail.com>
2023-07-27 08:45:42 +0900

    WHERE-Criteria 쿼리빌더 테스트 코드 추가

---
+++ src/test/java/org/example/jdbc/builder/WhereTest.java
@@ -0,0 +1,36 @@
+package org.example.jdbc.builder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.example.jdbc.builder.operator.Eq;
+import org.example.jdbc.builder.operator.Gt;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WhereTest {
+
+    @DisplayName("WHERE 절 생성 테스트")
+    @Test
+    void WhereTest() {
+        // given when
+        Where where = Where.builder()
+            .where(new Eq("VOUCHER_ID", "1"))
+            .build();
+
+        // then
+        assertEquals("WHERE VOUCHER_ID = 1", where.getQuery());
+    }
+
+    @DisplayName("WHERE 절 논리 조건문 추가 생성 테스트")
+    @Test
+    void WhereWithLogicalConditionTest() {
+        // given when
+        Where where = Where.builder()
+            .where(new Eq("VOUCHER_TYPE", "WELCOME"))
+            .and(new Gt("CUSTOMER_AGE", "20"))
+            .or(new Eq("CUSTOMER_TYPE", "VIP"))
+            .build();
+
+        // then
+        assertEquals("WHERE VOUCHER_TYPE = WELCOME AND CUSTOMER_AGE >= 20 OR CUSTOMER_TYPE = VIP", where.getQuery());
+    }
+}
